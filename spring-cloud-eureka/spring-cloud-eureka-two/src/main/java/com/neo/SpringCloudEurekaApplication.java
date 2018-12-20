69e5ff872f7ed1940a537d315ca2c651fd08c903
lyhq <947167863@qq.com>
2018-12-20 15:08:49 +0800

    SpringCloud-eureka双节点注册中心

--- spring-cloud-eureka/spring-cloud-eureka-two/src/main/java/com/neo/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/spring-cloud-eureka-two/src/main/java/com/neo/SpringCloudEurekaApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 双节点注册中心
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringCloudEurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudEurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
 }
