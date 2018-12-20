0398f453a12e0658802d466b96edb41abad68112
lyhq <947167863@qq.com>
2018-12-20 14:17:21 +0800

    SpringCloud-eureka单机版

--- spring-cloud-eureka/spring-cloud-eureka/src/main/java/com/neo/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/spring-cloud-eureka/src/main/java/com/neo/SpringCloudEurekaApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 主启动类
+ */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //启动注解版的EurekaServer功能
 public class SpringCloudEurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudEurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
 }
