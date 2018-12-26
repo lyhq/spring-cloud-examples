d6f60c0c8fd5e410caa3e422825d5440d1817e2a
lyhq <947167863@qq.com>
2018-12-26 16:33:37 +0800

    spring-cloud-config-git,config-repo

--- spring-cloud-config-git/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
+++ spring-cloud-config-git/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
@@ -1,15 +1,14 @@
 package com.neo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
+@EnableConfigServer //激活对配置中心的支持
 @SpringBootApplication
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
