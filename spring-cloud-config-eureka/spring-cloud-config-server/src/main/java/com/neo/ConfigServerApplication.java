2ffe5d4224e1e75e815d7084893225754ded5d38
lyhq <947167863@qq.com>
2018-12-27 11:05:37 +0800

    spring-cloud-config-eureka

--- spring-cloud-config-eureka/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
+++ spring-cloud-config-eureka/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
@@ -1,13 +1,12 @@
 package com.neo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableDiscoveryClient
-@EnableConfigServer
+@EnableDiscoveryClient	//启用服务的注册与发现
+@EnableConfigServer	//激活对配置中心的支持
 @SpringBootApplication
 public class ConfigServerApplication {
 
